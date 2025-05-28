602f124dcc87784e6ddb467bd7394c5bac98e4cc
LEDS Core <tadeuaugustocontato@gmail.com>
2025-05-28 05:08:56 +0000

    teste login

--- pages/Home Page/jsobjects/PageLoad/PageLoad.js
+++ pages/Home Page/jsobjects/PageLoad/PageLoad.js
@@ -1,7 +1,7 @@
 export default {
-  myFun1: () => {
-    if (!appsmith.store.user || Object.keys(appsmith.store.user).length === 0) {
-      navigateTo("Authentication", "SAME_WINDOW");
-    }
-  }
-}
+	myFun1: () => {
+		// Verifica se NÃO existe usuário logado
+		if (!appsmith.store.user || !appsmith.store.user.email) {
+			navigateTo("authentication-6824f0995d08da37cb92823f", "SAME_WINDOW");
+		}
+	}

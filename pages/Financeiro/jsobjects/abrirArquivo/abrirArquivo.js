a416f0361a35f7a300243b59c4c1619ee501a6a4
LEDS Core <tadeuaugustocontato@gmail.com>
2025-06-11 21:22:00 +0000

    Atualizações

--- pages/Financeiro/jsobjects/abrirArquivo/abrirArquivo.js
+++ pages/Financeiro/jsobjects/abrirArquivo/abrirArquivo.js
@@ -1,5 +1,11 @@
-export default {
-  abrir: (url) => {
-    navigateTo(url, "_blank");
-  }
-}
+{{ 
+  (() => {
+    const url = currentRow.url;
+    const win = window.open(url, '_blank');
+    if (win) {
+      win.focus();
+    } else {
+      showAlert("Pop-up bloqueado. Permita pop-ups no navegador.", "warning");
+    }
+  })()
+}}
